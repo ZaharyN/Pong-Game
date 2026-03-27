cebf5f20c6246d93d18f44f2d8e69316b165fc75
ZaharyN <zlnyagolov@abv.bg>
2026-03-27 11:33:18 +0200

    Separated CheckCollisions method for readability

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -189,159 +189,176 @@ void GameManager::Update(float deltaT)
 
 void GameManager::CheckCollisions()
 {
-	if (gameState == GameState::Playing)
+	if (gameState != GameState::Playing) return;
+
+	CheckWallCollisions();
+	CheckPaddleCollisions();
+	CheckBuddyCollisions();
+	CheckObstacleCollisions();
+	CheckDeadZone();
+	CheckCollectibleCollisions();
+}
+
+void GameManager::CheckWallCollisions()
+{
+	if (ball->GetBody().getPosition().x + ballRadius >= windowWidth)
 	{
-		// 1.Hits wall
-		if (ball->GetBody().getPosition().x + ballRadius >= windowWidth)
+		ball->SetPosition({ windowWidth - ballRadius, ball->GetBody().getPosition().y });
+		ball->SwapHorizontalDirection();
+		ball->ResetCurvature();
+		audioManager->PlaySound("hit");
+	}
+	else if (ball->GetBody().getPosition().x - ballRadius <= 0)
+	{
+		ball->SetPosition({ ballRadius, ball->GetBody().getPosition().y });
+		ball->SwapHorizontalDirection();
+		ball->ResetCurvature();
+		audioManager->PlaySound("hit");
+	}
+}
+
+void GameManager::CheckPaddleCollisions()
+{
+	if (ball->GetVerticalDirection() > 0 && ball->GetGlobalBounds().findIntersection(player1->GetGlobalBounds()))
+	{
+		ball->SetPosition({ ball->GetBody().getPosition().x, player1->GetBody().getPosition().y - (ballRadius + playerHeight / 2.f) - 1 });
+		ball->ApplySpin(player1->GetXDirection(), player1->GetSpinMultiplier(), player1->GetCurvaturePower());
+		ball->IncreaseSpeed();
+
+		if (player2->HasForesight())
+			player2->ComputeForesight(*ball, windowWidth, windowHeight);
+
+		audioManager->PlaySound("hit");
+		audioManager->SetPitch(ball->GetCurrentSpeed() / ball->GetInitialSpeed());
+
+		player1->UpdateEnergy(-1);
+	}
+	else if (ball->GetVerticalDirection() < 0 && ball->GetGlobalBounds().findIntersection(player2->GetGlobalBounds()))
+	{
+		ball->SetPosition({ ball->GetBody().getPosition().x, player2->GetBody().getPosition().y + (ballRadius + playerHeight / 2.f) + 1 });
+		ball->ApplySpin(player2->GetXDirection(), player2->GetSpinMultiplier(), player2->GetCurvaturePower());
+		ball->IncreaseSpeed();
+
+		if (player1->HasForesight())
+			player1->ComputeForesight(*ball, windowWidth, windowHeight);
+
+		audioManager->PlaySound("hit");
+		audioManager->SetPitch(ball->GetCurrentSpeed() / ball->GetInitialSpeed());
+
+		player2->UpdateEnergy(-1);
+	}
+}
+
+void GameManager::CheckBuddyCollisions()
+{
+	if (ball->GetVerticalDirection() > 0)
+	{
+		for (const auto& buddy : player1->GetBuddies())
 		{
-			ball->SetPosition({ windowWidth - ballRadius, ball->GetBody().getPosition().y });
-			ball->SwapHorizontalDirection();
-			ball->ResetCurvature();
-			audioManager->PlaySound("hit");
-		}
-		else if (ball->GetBody().getPosition().x - ballRadius <= 0)
-		{
-			ball->SetPosition({ ballRadius, ball->GetBody().getPosition().y });
-			ball->SwapHorizontalDirection();
-			ball->ResetCurvature();
-			audioManager->PlaySound("hit");
-		}
-
-		// 2.Ricochets from player
-		if (ball->GetVerticalDirection() > 0 && ball->GetGlobalBounds().findIntersection(player1->GetGlobalBounds()))
-		{
-			ball->SetPosition({ ball->GetBody().getPosition().x, player1->GetBody().getPosition().y - (ballRadius + playerHeight / 2.f) - 1 });
-			ball->ApplySpin(player1->GetXDirection(), player1->GetSpinMultiplier(), player1->GetCurvaturePower());
-			ball->IncreaseSpeed();
-
-			if (player2->HasForesight())
-				player2->ComputeForesight(*ball, windowWidth, windowHeight);
-
-			audioManager->PlaySound("hit");
-			audioManager->SetPitch(ball->GetCurrentSpeed() / ball->GetInitialSpeed());
-
-			player1->UpdateEnergy(-1);
-		}
-		else if (ball->GetVerticalDirection() < 0 && ball->GetGlobalBounds().findIntersection(player2->GetGlobalBounds()))
-		{
-			ball->SetPosition({ ball->GetBody().getPosition().x, player2->GetBody().getPosition().y + (ballRadius + playerHeight / 2.f) + 1 });
-			ball->ApplySpin(player2->GetXDirection(), player2->GetSpinMultiplier(), player2->GetCurvaturePower());
-			ball->IncreaseSpeed();
-
-			if (player1->HasForesight())
-				player1->ComputeForesight(*ball, windowWidth, windowHeight);
-
-			audioManager->PlaySound("hit");
-			audioManager->SetPitch(ball->GetCurrentSpeed() / ball->GetInitialSpeed());
-
-			player2->UpdateEnergy(-1);
-		}
-
-		// 3. Check buddies collisions
-		if (ball->GetVerticalDirection() > 0)
-		{
-			for (const auto& buddy : player1->GetBuddies())
+			if (buddy->GetGlobalBounds().findIntersection(ball->GetGlobalBounds()))
 			{
-				if (buddy->GetGlobalBounds().findIntersection(ball->GetGlobalBounds()))
-				{
-					ball->SetPosition({ ball->GetBody().getPosition().x, buddy->GetBody().getPosition().y - (ballRadius + playerHeight / 2.f) - 1 });
-					ball->SwapVerticalDirection();
-					ball->IncreaseSpeed();
+				ball->SetPosition({ ball->GetBody().getPosition().x, buddy->GetBody().getPosition().y - (ballRadius + playerHeight / 2.f) - 1 });
+				ball->SwapVerticalDirection();
+				ball->IncreaseSpeed();
 
-					if (player2->HasForesight())
-						player2->ComputeForesight(*ball, windowWidth, windowHeight);
+				if (player2->HasForesight())
+					player2->ComputeForesight(*ball, windowWidth, windowHeight);
 
-					audioManager->PlaySound("hit");
-					break;
-				}
+				audioManager->PlaySound("hit");
+				break;
 			}
 		}
-		if (ball->GetVerticalDirection() < 0)
+	}
+	if (ball->GetVerticalDirection() < 0)
+	{
+		for (const auto& buddy : player2->GetBuddies())
 		{
-			for (const auto& buddy : player2->GetBuddies())
+			if (buddy->GetGlobalBounds().findIntersection(ball->GetGlobalBounds()))
 			{
-				if (buddy->GetGlobalBounds().findIntersection(ball->GetGlobalBounds()))
-				{
-					ball->SetPosition({ ball->GetBody().getPosition().x, buddy->GetBody().getPosition().y + (ballRadius + playerHeight / 2.f) + 1 });
-					ball->SwapVerticalDirection();
-					ball->IncreaseSpeed();
+				ball->SetPosition({ ball->GetBody().getPosition().x, buddy->GetBody().getPosition().y + (ballRadius + playerHeight / 2.f) + 1 });
+				ball->SwapVerticalDirection();
+				ball->IncreaseSpeed();
 
-					if (player1->HasForesight())
-						player1->ComputeForesight(*ball, windowWidth, windowHeight);
+				if (player1->HasForesight())
+					player1->ComputeForesight(*ball, windowWidth, windowHeight);
 
-					audioManager->PlaySound("hit");
-					break;
-				}
+				audioManager->PlaySound("hit");
+				break;
 			}
 		}
+	}
+}
 
-		// 4. Check obstacle collisions
-		// Check if ball is towards top player
-		if (ball->GetVerticalDirection() < 0)
+void GameManager::CheckObstacleCollisions()
+{
+	if (ball->GetVerticalDirection() < 0)
+	{
+		for (const auto& obs : player2->GetObstacles())
 		{
-			for (const auto& obs : player2->GetObstacles())
+			if (ball->GetGlobalBounds().findIntersection(obs.getGlobalBounds()))
 			{
-				if (ball->GetGlobalBounds().findIntersection(obs.getGlobalBounds()))
-				{
-					ball->SwapVerticalDirection();
-					audioManager->PlaySound("hit");
-					break;
-				}
+				ball->SwapVerticalDirection();
+				audioManager->PlaySound("hit");
+				break;
 			}
 		}
-		// Check if ball is towards bottom player
-		if (ball->GetVerticalDirection() > 0)
+	}
+	// Check if ball is towards bottom player
+	if (ball->GetVerticalDirection() > 0)
+	{
+		for (const auto& obs : player1->GetObstacles())
 		{
-			for (const auto& obs : player1->GetObstacles())
+			if (ball->GetGlobalBounds().findIntersection(obs.getGlobalBounds()))
 			{
-				if (ball->GetGlobalBounds().findIntersection(obs.getGlobalBounds()))
-				{
-					ball->SwapVerticalDirection();
-					audioManager->PlaySound("hit");
-					break;
-				}
+				ball->SwapVerticalDirection();
+				audioManager->PlaySound("hit");
+				break;
 			}
 		}
+	}
+}
 
-		// 5.Hits dead zone
-		if (ball->GetBody().getPosition().y - ballRadius <= 0
-			|| ball->GetBody().getPosition().y + ballRadius >= windowHeight)
-		{
-			player1->Reset();
-			player2->Reset();
-			ball->Reset();
-			collectibleManager->Reset();
-			audioManager->StopBackgroundMusic();
+void GameManager::CheckDeadZone()
+{
+	if (ball->GetBody().getPosition().y - ballRadius <= 0
+		|| ball->GetBody().getPosition().y + ballRadius >= windowHeight)
+	{
+		player1->Reset();
+		player2->Reset();
+		ball->Reset();
+		collectibleManager->Reset();
+		audioManager->StopBackgroundMusic();
 
-			gameState = GameState::Menu;
-			return;
-		}
+		gameState = GameState::Menu;
+		return;
+	}
+}
 
-		// 6. Check collectibles pickup
-		collectibleManager->CheckCollisions(player1.get(), player2.get(), *audioManager);
+void GameManager::CheckCollectibleCollisions()
+{
+	collectibleManager->CheckCollisions(player1.get(), player2.get(), *audioManager);
 
-		if (player1->GetCollectedEnergy() == collectibleCountForUpgrade)
-		{
-			player1->ResetCollectedEnergy();
-			upgradeRecipient = player1.get();
-			upgradeVictim = player2.get();
+	if (player1->GetCollectedEnergy() == collectibleCountForUpgrade)
+	{
+		player1->ResetCollectedEnergy();
+		upgradeRecipient = player1.get();
+		upgradeVictim = player2.get();
 
-			currentUpgradeOptions = upgradeManager->ChooseThreeRandomUpgrades(player1->GetOwnedUniqueUpgrades());
-			uiManager->ShowRandomUpgrades(currentUpgradeOptions);
+		currentUpgradeOptions = upgradeManager->ChooseThreeRandomUpgrades(player1->GetOwnedUniqueUpgrades());
+		uiManager->ShowRandomUpgrades(currentUpgradeOptions, "PLAYER 1");
 
-			gameState = GameState::UpgradeSelect;
-		}
-		else if (player2->GetCollectedEnergy() == collectibleCountForUpgrade)
-		{
-			player2->ResetCollectedEnergy();
-			upgradeRecipient = player2.get();
-			upgradeVictim = player1.get();
+		gameState = GameState::UpgradeSelect;
+	}
+	else if (player2->GetCollectedEnergy() == collectibleCountForUpgrade)
+	{
+		player2->ResetCollectedEnergy();
+		upgradeRecipient = player2.get();
+		upgradeVictim = player1.get();
 
-			currentUpgradeOptions = upgradeManager->ChooseThreeRandomUpgrades(player2->GetOwnedUniqueUpgrades());
-			uiManager->ShowRandomUpgrades(currentUpgradeOptions);
+		currentUpgradeOptions = upgradeManager->ChooseThreeRandomUpgrades(player2->GetOwnedUniqueUpgrades());
+		uiManager->ShowRandomUpgrades(currentUpgradeOptions, "PLAYER 2");
 
-			gameState = GameState::UpgradeSelect;
-		}
+		gameState = GameState::UpgradeSelect;
 	}
 }
 
